f38c1e8f48f58fb5ab5d8ec206bd40c4cccd7610
Marel Oliva <marel.oliva@gmail.com>
2014-08-10 12:25:07 -0400

    Added TextRamModelUtil to transform external aspects, transform individual aspect, serialize, et

---
+++ tool/cl.uchile.pleiad.textram/src/cl/uchile/pleiad/util/TextRamModelUtil.java
@@ -0,0 +1,71 @@
+package cl.uchile.pleiad.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.xtext.serializer.impl.Serializer;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import cl.uchile.pleiad.textRam.TAspect;
+import cl.uchile.pleiad.textRam.TInstantiationHeader;
+import cl.uchile.pleiad.transform.ITextRAMTransform;
+import cl.uchile.pleiad.transform.TextRAMTransform;
+import ca.mcgill.cs.sel.ram.Aspect;
+import ca.mcgill.cs.sel.ram.Instantiation;
+import ca.mcgill.cs.sel.ram.InstantiationType;
+import ca.mcgill.cs.sel.ram.RamPackage;
+import ca.mcgill.sel.commons.emf.util.ResourceManager;
+
+public class TextRamModelUtil {
+
+	private TextRamModelUtil() {}
+	
+	private static Aspect loadModelFromFile(String file) {
+		if (!EPackage.Registry.INSTANCE.containsKey(RamPackage.eINSTANCE.getNsURI())) {
+			EPackage.Registry.INSTANCE.put(RamPackage.eINSTANCE.getNsURI(), ca.mcgill.cs.sel.ram.RamPackage.eINSTANCE);
+		}
+		
+		Aspect res = (Aspect)ResourceManager.loadModel(file);
+		
+		return res;
+	}
+	
+	public static TAspect transformToTextRamAspect( Aspect ramAspect ) {
+		ITextRAMTransform transformer = new TextRAMTransform();
+		
+		TAspect res =  transformer.transform(ramAspect);
+		
+		return res;
+	}
+	
+	public static String serialize(TAspect a) {
+		Injector injector = Guice.createInjector(new  cl.uchile.pleiad.TextRAMRuntimeModule());  
+		Serializer serializer = injector.getInstance(Serializer.class);  
+		
+		String res = serializer.serialize(a);
+		
+		return res;
+	}
+	
+	public static Set<TAspect> transformExtendedAspects( Aspect aspect ) {
+		Set<TAspect> res = new HashSet<TAspect>();
+		
+		for ( Instantiation instantiation : aspect.getInstantiations()) {
+			if ( instantiation.getType() == InstantiationType.EXTENDS ) {
+				Aspect externalAspect = instantiation.getExternalAspect();
+				
+				// If it was already added, it not necessary to search for extended aspects of that aspect again
+				// this prevents infinite loops in case of cyclic dependencies between aspects
+				if (res.add( transformToTextRamAspect( externalAspect )) ) {
+					res.addAll(  transformExtendedAspects( externalAspect ) );
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+}
